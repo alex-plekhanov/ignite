c06ce15cb0b59d54c58db199c57622c65c52b976
Aleksey Plekhanov <plehanov.alex@gmail.com>
2018-03-14 15:37:11 +0300

    Distributed meta views. WIP

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/views/IgniteSqlDistributedMetaViewAdapter.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/views/IgniteSqlDistributedMetaViewAdapter.java
@@ -17,20 +17,16 @@
 
 package org.apache.ignite.internal.processors.query.h2.views;
 
+import java.util.Iterator;
 import java.util.UUID;
-import org.apache.ignite.IgniteCheckedException;
-import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.internal.GridKernalContext;
-import org.apache.ignite.internal.GridTopic;
-import org.apache.ignite.internal.managers.communication.GridIoPolicy;
-import org.apache.ignite.internal.managers.communication.GridMessageListener;
-import org.apache.ignite.internal.util.typedef.internal.U;
-import org.apache.ignite.lang.IgniteBiTuple;
 import org.h2.engine.Session;
 import org.h2.result.Row;
 import org.h2.result.SearchRow;
 import org.h2.table.Column;
 import org.h2.value.Value;
+import org.h2.value.ValueUuid;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Distributed meta view adapter.
@@ -38,12 +34,6 @@ import org.h2.value.Value;
 public class IgniteSqlDistributedMetaViewAdapter extends IgniteSqlAbstractMetaView {
     /** Delegate view. */
     protected final IgniteSqlMetaView delegate;
-
-    /** Message topic. */
-    protected final Object msgTopic;
-
-    /** Message listener. */
-    protected final GridMessageListener msgLsnr;
 
     /**
      * @param cols Columns.
@@ -82,72 +72,85 @@ public class IgniteSqlDistributedMetaViewAdapter extends IgniteSqlAbstractMetaVi
             addDistributedIndexes(delegate.getIndexes()));
 
         this.delegate = delegate;
-        this.msgTopic = new IgniteBiTuple<>(GridTopic.TOPIC_QUERY, delegate.getTableName());
-
-        msgLsnr = new GridMessageListener() {
-            @Override public void onMessage(UUID nodeId, Object msg, byte plc) {
-                onMessage0(nodeId, msg);
-            }
-        };
-
-        ctx.io().addMessageListener(msgTopic, msgLsnr);
-    }
-
-    /**
-     * @param nodeId Source node Id.
-     * @param msg Message.
-     */
-    private void onMessage0(UUID nodeId, Object msg) {
-        ClusterNode node = ctx.discovery().node(nodeId);
-
-        if (node == null)
-            return;
-
-        try {
-            if (msg instanceof MetaViewRowsRequest)
-                onRowsRequest(node, (MetaViewRowsRequest)msg);
-            else if (msg instanceof MetaViewRowsResponse)
-                onRowsResponse(node, (MetaViewRowsResponse)msg);
-        }
-        catch (Throwable th) {
-            U.error(log, "Failed to handle message [nodeId=" + nodeId + ", msg=" + msg + "]", th);
-
-            if (th instanceof Error)
-                throw th;
-        }
-    }
-
-    /**
-     * @param node Node.
-     * @param msg Message.
-     */
-    protected void onRowsRequest(ClusterNode node, MetaViewRowsRequest msg) {
-        // TODO: get SearchRow from request, invoke getRows, send response
-    }
-
-    /**
-     * @param node Node.
-     * @param msg Message.
-     */
-    protected void onRowsResponse(ClusterNode node, MetaViewRowsResponse msg) {
-        // TODO: update future
     }
 
     /** {@inheritDoc} */
     @Override public Iterable<Row> getRows(Session ses, SearchRow first, SearchRow last) {
-        // TODO: send request, get future, create iterable based on future
+        return new NodeRowIterable(delegate.getRows(ses, first, last), ctx.localNodeId(), ses);
+    }
 
-/*
-        for (ClusterNode node : ctx.discovery().allNodes()) {
-            try {
-                ctx.io().sendOrderedMessage(node, msgTopic, new MetaViewRowsRequest(), GridIoPolicy.QUERY_POOL, 0, false);
-            }
-            catch (IgniteCheckedException e) {
-                e.printStackTrace();
-            }
+    /**
+     * Row iterable with nodeId column.
+     */
+    private static class NodeRowIterable implements Iterable<Row> {
+        /** Delegate. */
+        private final Iterable<Row> delegate;
+
+        /** Node id. */
+        private final UUID nodeId;
+
+        /** Session. */
+        private final Session ses;
+
+        /**
+         * Default constructor.
+         */
+        public NodeRowIterable(Iterable<Row> delegate, UUID nodeId, Session ses) {
+            this.delegate = delegate;
+            this.nodeId = nodeId;
+            this.ses = ses;
         }
-*/
 
-        return null;
+        /** {@inheritDoc} */
+        @NotNull @Override public Iterator<Row> iterator() {
+            return new NodeRowIterator(delegate.iterator(), nodeId, ses);
+        }
+    }
+
+    /**
+     * Row iterator with nodeId column.
+     */
+    private static class NodeRowIterator implements Iterator<Row> {
+        /** Delegate. */
+        private final Iterator<Row> delegate;
+
+        /** Node id. */
+        private final UUID nodeId;
+
+        /** Session. */
+        private final Session ses;
+
+        /**
+         * @param delegate Delegate.
+         * @param nodeId Node id.
+         * @param ses Database session
+         */
+        private NodeRowIterator(Iterator<Row> delegate, UUID nodeId, Session ses) {
+            this.delegate = delegate;
+            this.nodeId = nodeId;
+            this.ses = ses;
+        }
+
+        /** {@inheritDoc} */
+        @Override public boolean hasNext() {
+            return delegate.hasNext();
+        }
+
+        /** {@inheritDoc} */
+        @Override public Row next() {
+            Row row = delegate.next();
+
+            if (row == null)
+                return null;
+
+            Value[] oldValues = row.getValueList();
+            Value[] newValues = new Value[oldValues.length + 1];
+
+            newValues[0] = ValueUuid.get(nodeId.toString());
+
+            System.arraycopy(oldValues, 0, newValues, 1, oldValues.length);
+
+            return ses.getDatabase().createRow(newValues, 1);
+        }
     }
 }
