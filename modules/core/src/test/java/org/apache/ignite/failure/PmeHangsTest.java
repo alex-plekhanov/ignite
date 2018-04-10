766655d9c700b0241efb375a06b9159c4c1140ee
Aleksey Plekhanov <Plehanov.Alex@gmail.com>
2018-04-10 22:19:03 +0300

    IGNITE-8166 PME hangs when error occurs during checkpoint

---
+++ modules/core/src/test/java/org/apache/ignite/failure/PmeHangsTest.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.failure;
+
+import org.apache.ignite.IgniteCheckedException;
+import org.apache.ignite.configuration.DataRegionConfiguration;
+import org.apache.ignite.configuration.DataStorageConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.processors.cache.persistence.DbCheckpointListener;
+import org.apache.ignite.internal.processors.cache.persistence.GridCacheDatabaseSharedManager;
+import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+
+/**
+ * PME hangs test.
+ */
+public class PmeHangsTest extends GridCommonAbstractTest {
+    /** {@inheritDoc} */
+    @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
+        IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
+
+        cfg.setDataStorageConfiguration(
+            new DataStorageConfiguration().setDefaultDataRegionConfiguration(
+                new DataRegionConfiguration().setPersistenceEnabled(true)
+            )
+        );
+
+        return cfg;
+    }
+
+    /**
+     * PME hangs test
+     */
+    public void testPmeHangs() throws Exception {
+        cleanPersistenceDir();
+
+        IgniteEx ignite = startGrid(0);
+
+        ((GridCacheDatabaseSharedManager)ignite.context().cache().context().database()).addCheckpointListener(
+            new DbCheckpointListener() {
+                @Override public void onCheckpointBegin(Context ctx) throws IgniteCheckedException {
+                    throw new IllegalStateException();
+                }
+            }
+        );
+
+        ignite.cluster().active(true);
+    }
+}
