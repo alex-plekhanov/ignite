3aade1078407fa46df7a8dea4c501448edb4ee55
Aleksey Plekhanov <plehanov.alex@gmail.com>
2019-02-14 16:23:41 +0300

    Cache metrics put and commit avg time from different nodes

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/near/GridCachePartitionedMetricsSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/near/GridCachePartitionedMetricsSelfTest.java
@@ -17,10 +17,14 @@
 
 package org.apache.ignite.internal.processors.cache.distributed.near;
 
+import org.apache.ignite.IgniteCache;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.internal.IgniteEx;
 import org.apache.ignite.internal.processors.cache.GridCacheTransactionalAbstractMetricsSelfTest;
 import org.apache.ignite.testframework.MvccFeatureChecker;
+import org.apache.ignite.transactions.Transaction;
+import org.junit.Test;
 
 import static org.apache.ignite.cache.CacheMode.PARTITIONED;
 import static org.apache.ignite.cache.CacheRebalanceMode.SYNC;
@@ -54,7 +58,7 @@ public class GridCachePartitionedMetricsSelfTest extends GridCacheTransactionalA
         CacheConfiguration cfg = super.cacheConfiguration(igniteInstanceName);
 
         cfg.setCacheMode(PARTITIONED);
-        cfg.setBackups(gridCount() - 1);
+        cfg.setBackups(1);
         cfg.setRebalanceMode(SYNC);
         cfg.setWriteSynchronizationMode(FULL_SYNC);
 
@@ -63,6 +67,46 @@ public class GridCachePartitionedMetricsSelfTest extends GridCacheTransactionalA
 
     /** {@inheritDoc} */
     @Override protected int gridCount() {
-        return GRID_CNT;
+        return 3;
     }
+
+
+    @Test
+    public void testPuts() throws Exception {
+        IgniteEx cl = startGrid(getConfiguration("client").setClientMode(true));
+
+        final IgniteCache<Integer, Integer> cache0 = grid(0).cache(DEFAULT_CACHE_NAME);
+        final IgniteCache<Integer, Integer> cache1 = grid(1).cache(DEFAULT_CACHE_NAME);
+        final IgniteCache<Integer, Integer> cacheCl = cl.cache(DEFAULT_CACHE_NAME);
+
+        for (int i = 0; i < 500; i++)
+            cache0.put(i, i);
+
+        cache1.put(0, 0);
+        //cacheCl.put(0, 0);
+
+        Transaction tx = cl.transactions().txStart();
+
+        cacheCl.put(0, 0);
+
+        doSleep(500L);
+
+        tx.commit();
+
+        doSleep(1_000L);
+
+        log.info(">>>> Puts0: " + cache0.localMetrics().getCachePuts());
+        log.info(">>>> Puts1: " + cache1.localMetrics().getCachePuts());
+        log.info(">>>> PutsCl: " + cacheCl.localMetrics().getCachePuts());
+        log.info(">>>> AvgPuts0: " + cache0.localMetrics().getAveragePutTime());
+        log.info(">>>> AvgPuts1: " + cache1.localMetrics().getAveragePutTime());
+        log.info(">>>> AvgPutsCl: " + cacheCl.localMetrics().getAveragePutTime());
+        log.info(">>>> Commit0: " + cache0.localMetrics().getCacheTxCommits());
+        log.info(">>>> Commit1: " + cache1.localMetrics().getCacheTxCommits());
+        log.info(">>>> CommitCl: " + cacheCl.localMetrics().getCacheTxCommits());
+        log.info(">>>> AvgCommit0: " + cache0.localMetrics().getAverageTxCommitTime());
+        log.info(">>>> AvgCommit1: " + cache1.localMetrics().getAverageTxCommitTime());
+        log.info(">>>> AvgCommitCl: " + cacheCl.localMetrics().getAverageTxCommitTime());
+    }
+
 }
