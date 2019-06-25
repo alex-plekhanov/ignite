c8638cda249f15aa36fc2fc606c00c205e00572e
Aleksey Plekhanov <plehanov.alex@gmail.com>
2019-06-25 12:15:11 +0300

    IGNITE-11857 Benchmark

--- modules/benchmarks/src/main/java/org/apache/ignite/internal/benchmarks/jmh/misc/PartitionUpdateCounterBenchmark.java
+++ modules/benchmarks/src/main/java/org/apache/ignite/internal/benchmarks/jmh/misc/PartitionUpdateCounterBenchmark.java
@@ -1,22 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.benchmarks.jmh.misc;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
-import org.apache.ignite.internal.benchmarks.jmh.runner.JmhIdeBenchmarkRunner;
 import org.apache.ignite.internal.processors.cache.PartitionTxUpdateCounterImpl;
 import org.apache.ignite.internal.processors.cache.PartitionUpdateCounter;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 /**
  * Benchmarks {@link PartitionTxUpdateCounterImpl} class.
  */
 @State(Scope.Benchmark)
+@Fork(1)
+@Threads(1)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5, time = 2)
+@Measurement(iterations = 10, time = 3)
 public class PartitionUpdateCounterBenchmark {
     /** Buffer size to store gaps. */
     private static final int GAPS_BUFFER_SIZE = 50;
@@ -72,13 +103,10 @@ public class PartitionUpdateCounterBenchmark {
      * @throws Exception Exception.
      */
     public static void main(String[] args) throws Exception {
-        JmhIdeBenchmarkRunner.create()
-            .threads(1)
-            .measurementIterations(10)
-            .warmupIterations(5)
-            .benchmarkModes(Mode.Throughput)
-            .outputTimeUnit(TimeUnit.MILLISECONDS)
-            .benchmarks(PartitionUpdateCounterBenchmark.class.getSimpleName())
-            .run();
+        final Options options = new OptionsBuilder()
+            .include(PartitionUpdateCounterBenchmark.class.getSimpleName())
+            .build();
+
+        new Runner(options).run();
     }
 }
