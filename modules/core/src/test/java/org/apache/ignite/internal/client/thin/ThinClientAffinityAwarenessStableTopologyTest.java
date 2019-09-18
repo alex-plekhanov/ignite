bb04932f36b6299d11fb2c1f5c2853c2e682c045
Aleksey Plekhanov <plehanov.alex@gmail.com>
2019-09-18 10:45:09 +0300

    IGNITE-11898 WIP

--- modules/core/src/test/java/org/apache/ignite/internal/client/thin/ThinClientAffinityAwarenessStableTopologyTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/client/thin/ThinClientAffinityAwarenessStableTopologyTest.java
@@ -18,27 +18,14 @@
 package org.apache.ignite.internal.client.thin;
 
 import java.util.Collection;
-import java.util.Queue;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.function.Consumer;
 import java.util.function.Function;
-import org.apache.ignite.cache.CacheKeyConfiguration;
-import org.apache.ignite.cache.CacheMode;
-import org.apache.ignite.cache.affinity.AffinityKeyMapped;
-import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
-import org.apache.ignite.client.ClientAuthorizationException;
 import org.apache.ignite.client.ClientCache;
-import org.apache.ignite.client.ClientConnectionException;
-import org.apache.ignite.client.IgniteClient;
 import org.apache.ignite.cluster.ClusterNode;
-import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.ClientConfiguration;
-import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.processors.cache.IgniteInternalCache;
 import org.apache.ignite.internal.util.typedef.T2;
 import org.apache.ignite.testframework.GridTestUtils;
-import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
 
 import static org.apache.ignite.configuration.ClientConnectorConfiguration.DFLT_PORT;
@@ -46,61 +33,9 @@ import static org.apache.ignite.configuration.ClientConnectorConfiguration.DFLT_
 /**
  * Test affinity awareness of thin client on stable topology.
  */
-public class ThinClientAffinityAwarenessStableTopologyTest extends GridCommonAbstractTest {
-    /** Replicated cache name. */
-    private static final String REPL_CACHE_NAME = "replicated_cache";
-
-    /** Partitioned cache name. */
-    private static final String PART_CACHE_NAME = "partitioned_cache";
-
-    /** Partitioned with custom affinity cache name. */
-    private static final String PART_CUSTOM_AFFINITY_CACHE_NAME = "partitioned_custom_affinity_cache";
-
-    /** Keys count. */
-    private static final int KEY_CNT = 30;
-
-    /** Cluster size. */
-    private static final int CLUSTER_SIZE = 4;
-
+public class ThinClientAffinityAwarenessStableTopologyTest extends ThinClientAbstractAffinityAwarenessTest {
     /** Wait timeout. */
     private static final long WAIT_TIMEOUT = 1_000L;
-
-    /** Channels. */
-    private final TestTcpClientChannel channels[] = new TestTcpClientChannel[CLUSTER_SIZE];
-
-    /** Operations queue. */
-    private final Queue<T2<TestTcpClientChannel, ClientOperation>> opsQueue = new ConcurrentLinkedQueue<>();
-
-    /** Default channel. */
-    private TestTcpClientChannel dfltCh;
-
-    /** Client instance. */
-    private IgniteClient client;
-
-    /** {@inheritDoc} */
-    @Override protected IgniteConfiguration getConfiguration(String igniteInstanceName) throws Exception {
-        IgniteConfiguration cfg = super.getConfiguration(igniteInstanceName);
-
-        cfg.setConsistentId(igniteInstanceName);
-
-        CacheConfiguration ccfg0 = new CacheConfiguration()
-            .setName(REPL_CACHE_NAME)
-            .setCacheMode(CacheMode.REPLICATED);
-
-        CacheConfiguration ccfg1 = new CacheConfiguration()
-            .setName(PART_CUSTOM_AFFINITY_CACHE_NAME)
-            .setCacheMode(CacheMode.PARTITIONED)
-            .setAffinity(new CustomAffinityFunction());
-
-        CacheConfiguration ccfg2 = new CacheConfiguration()
-            .setName(PART_CACHE_NAME)
-            .setCacheMode(CacheMode.PARTITIONED)
-            .setKeyConfiguration(
-                new CacheKeyConfiguration(TestNotAnnotatedAffinityKey.class.getName(), "affinityKey"),
-                new CacheKeyConfiguration(TestAnnotatedAffinityKey.class));
-
-        return cfg.setCacheConfiguration(ccfg0, ccfg1, ccfg2);
-    }
 
     /** {@inheritDoc} */
     @Override protected void beforeTestsStarted() throws Exception {
@@ -142,13 +77,6 @@ public class ThinClientAffinityAwarenessStableTopologyTest extends GridCommonAbs
         assertEquals(nextChOp.get2(), ClientOperation.CACHE_GET_OR_CREATE_WITH_NAME);
 
         dfltCh = nextChOp.get1();
-    }
-
-    /** {@inheritDoc} */
-    @Override protected void afterTest() throws Exception {
-        super.afterTest();
-
-        opsQueue.clear();
     }
 
     /**
@@ -267,7 +195,7 @@ public class ThinClientAffinityAwarenessStableTopologyTest extends GridCommonAbs
 
         clientCache.put(keyFactory.apply(0), 0);
 
-        TestTcpClientChannel opCh = affinityChannel(keyFactory.apply(0), igniteCache, dfltCh);
+        TestTcpClientChannel opCh = affinityChannel(keyFactory.apply(0), igniteCache);
 
         // Default channel is the first who detects topology change, so next partition request will go through
         // the default channel.
@@ -277,7 +205,7 @@ public class ThinClientAffinityAwarenessStableTopologyTest extends GridCommonAbs
         for (int i = 1; i < KEY_CNT; i++) {
             Object key = keyFactory.apply(i);
 
-            opCh = affinityChannel(key, igniteCache, dfltCh);
+            opCh = affinityChannel(key, igniteCache);
 
             clientCache.put(key, key);
 
@@ -322,155 +250,6 @@ public class ThinClientAffinityAwarenessStableTopologyTest extends GridCommonAbs
             clientCache.putIfAbsent(key, i);
 
             assertOpOnChannel(opCh, ClientOperation.CACHE_PUT_IF_ABSENT);
-        }
-    }
-
-    /**
-     * Checks that operation goes through specified channel.
-     */
-    private void assertOpOnChannel(TestTcpClientChannel expCh, ClientOperation expOp) {
-        T2<TestTcpClientChannel, ClientOperation> nextChOp = opsQueue.poll();
-
-        assertNotNull("Unexpected (null) next operation [expCh=" + expCh + ", expOp=" + expOp + ']', nextChOp);
-
-        assertEquals("Unexpected channel for opertation [expCh=" + expCh + ", expOp=" + expOp +
-                ", nextOpCh=" + nextChOp + ']', expCh, nextChOp.get1());
-
-        assertEquals("Unexpected operation on channel [expCh=" + expCh + ", expOp=" + expOp +
-            ", nextOpCh=" + nextChOp + ']', expOp, nextChOp.get2());
-    }
-
-    /**
-     * Calculates affinity channel for cache and key.
-     */
-    private TestTcpClientChannel affinityChannel(Object key, IgniteInternalCache<Object, Object> cache, TestTcpClientChannel dfltCh) {
-        Collection<ClusterNode> nodes = cache.affinity().mapKeyToPrimaryAndBackups(key);
-
-        UUID nodeId = nodes.iterator().next().id();
-
-        for (int i = 0; i < channels.length; i++) {
-            if (channels[i] != null && nodeId.equals(channels[i].serverNodeId()))
-                return channels[i];
-        }
-
-        return dfltCh;
-    }
-
-    /**
-     * Test TCP client channel.
-     */
-    private class TestTcpClientChannel extends TcpClientChannel {
-        /** Closed. */
-        private volatile boolean closed;
-
-        /** Channel configuration. */
-        private final ClientChannelConfiguration cfg;
-
-        /**
-         * @param cfg Config.
-         */
-        public TestTcpClientChannel(ClientChannelConfiguration cfg) {
-            super(cfg);
-
-            this.cfg = cfg;
-
-            channels[cfg.getAddress().getPort() - DFLT_PORT] = this;
-        }
-
-        /** {@inheritDoc} */
-        @Override public <T> T service(ClientOperation op, Consumer<PayloadOutputChannel> payloadWriter,
-            Function<PayloadInputChannel, T> payloadReader) throws ClientConnectionException, ClientAuthorizationException {
-            // Store all operations except binary type registration in queue to check later.
-            if (op != ClientOperation.REGISTER_BINARY_TYPE_NAME &&  op != ClientOperation.PUT_BINARY_TYPE)
-                opsQueue.offer(new T2<>(this, op));
-
-            return super.service(op, payloadWriter, payloadReader);
-        }
-
-        /** {@inheritDoc} */
-        @Override public void close() throws Exception {
-            super.close();
-
-            closed = true;
-        }
-
-        /** {@inheritDoc} */
-        @Override public String toString() {
-            return cfg.getAddress().toString();
-        }
-    }
-
-    /**
-     *
-     */
-    private static class CustomAffinityFunction extends RendezvousAffinityFunction {
-        // No-op.
-    }
-
-    /**
-     * Test class without affinity key.
-     */
-    private static class TestComplexKey {
-        /** */
-        int firstField;
-
-        /** Another field. */
-        int secondField;
-
-        /** */
-        public TestComplexKey(int firstField, int secondField) {
-            this.firstField = firstField;
-            this.secondField = secondField;
-        }
-
-        /** {@inheritDoc} */
-        @Override public int hashCode() {
-            return firstField + secondField;
-        }
-    }
-
-    /**
-     * Test class with annotated affinity key.
-     */
-    private static class TestAnnotatedAffinityKey {
-        /** */
-        @AffinityKeyMapped
-        int affinityKey;
-
-        /** */
-        int anotherField;
-
-        /** */
-        public TestAnnotatedAffinityKey(int affinityKey, int anotherField) {
-            this.affinityKey = affinityKey;
-            this.anotherField = anotherField;
-        }
-
-        /** {@inheritDoc} */
-        @Override public int hashCode() {
-            return affinityKey + anotherField;
-        }
-    }
-
-    /**
-     * Test class with affinity key without annotation.
-     */
-    private static class TestNotAnnotatedAffinityKey {
-        /** */
-        TestComplexKey affinityKey;
-
-        /** */
-        int anotherField;
-
-        /** */
-        public TestNotAnnotatedAffinityKey(TestComplexKey affinityKey, int anotherField) {
-            this.affinityKey = affinityKey;
-            this.anotherField = anotherField;
-        }
-
-        /** {@inheritDoc} */
-        @Override public int hashCode() {
-            return affinityKey.hashCode() + anotherField;
         }
     }
 }
