8c81a6ea93f5a06bd54ffa83627d127a3bc39b7b
Aleksey Plekhanov <plehanov.alex@gmail.com>
2020-03-02 15:12:22 +0300

    IGN-308 Reproducer

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/CachePartitionEvictionQueryTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/CachePartitionEvictionQueryTest.java
@@ -17,9 +17,11 @@
 
 package org.apache.ignite.internal.processors.cache;
 
+import java.util.Iterator;
+import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteDataStreamer;
 import org.apache.ignite.Ignition;
-import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
 import org.apache.ignite.cache.query.ScanQuery;
 import org.apache.ignite.client.IgniteClient;
 import org.apache.ignite.configuration.CacheConfiguration;
@@ -38,19 +40,32 @@ import org.junit.Test;
  *
  */
 public class CachePartitionEvictionQueryTest extends GridCommonAbstractTest {
-    /** {@inheritDoc} */
+    private static final String[] IDS = new String[] {
+        "RubinNT_grid960.delta.sbrf.ru",
+        "RubinNT_grid952.delta.sbrf.ru",
+        "RubinNT_grid956.delta.sbrf.ru",
+        "RubinNT_grid959.delta.sbrf.ru"
+    };
+
+    /**
+     * {@inheritDoc}
+     */
     @Override protected IgniteConfiguration getConfiguration(String name) throws Exception {
         IgniteConfiguration cfg = super.getConfiguration(name);
 
-        cfg.setCacheConfiguration(new CacheConfiguration<Integer, Integer>(DEFAULT_CACHE_NAME)
-            .setBackups(0)
-            .setCacheMode(CacheMode.PARTITIONED)
-            .setIndexedTypes(Integer.class, Integer.class)
-        );
+        if (name.startsWith(getTestIgniteInstanceName())) {
+            int nodeIdx = getTestIgniteInstanceIndex(name);
+
+            cfg.setConsistentId(IDS[nodeIdx]);
+        }
+        else
+            cfg.setConsistentId(name);
 
         cfg.setDataStorageConfiguration(new DataStorageConfiguration()
-            .setDefaultDataRegionConfiguration(new DataRegionConfiguration().setPersistenceEnabled(true)));
-
+            .setDefaultDataRegionConfiguration(new DataRegionConfiguration()
+                .setPersistenceEnabled(true)
+                .setMaxSize(512L * 1024 * 1024)
+            ));
 
         return cfg;
     }
@@ -74,13 +89,86 @@ public class CachePartitionEvictionQueryTest extends GridCommonAbstractTest {
      */
     @Test
     public void testQuery() throws Exception {
-        startGrids(3);
+        startGrids(4);
 
         grid(0).cluster().active(true);
-/*
+
+        grid(0).getOrCreateCache(new CacheConfiguration<Integer, Integer>(DEFAULT_CACHE_NAME).setBackups(1));
+
+        try (IgniteDataStreamer<Integer, Integer> streamer = grid(0).dataStreamer(DEFAULT_CACHE_NAME)) {
+            for (int i = 0; i < 10_000; i++)
+                streamer.addData(i, i);
+        }
+
+        stopAllGrids();
+
+        startGrid(0);
+        startGrid(1);
+
+        grid(0).cluster().active(true);
+
+        startGrid(2);
+        startGrid(3);
+
         grid(0).cluster().baselineAutoAdjustEnabled(true);
-        grid(0).cluster().baselineAutoAdjustTimeout(5_000L);
-*/
+        grid(0).cluster().baselineAutoAdjustTimeout(10L);
+
+        IgniteClient client = Ignition.startClient(new ClientConfiguration().setAddresses("127.0.0.1:10800"));
+
+        while (true) {
+            stopGrid(1);
+
+            //grid(0).cluster().setBaselineTopology(grid(0).cluster().topologyVersion());
+
+            //awaitPartitionMapExchange();
+
+            doSleep(10_000L);
+
+            stopGrid(2);
+
+            //grid(0).cluster().setBaselineTopology(grid(0).cluster().topologyVersion());
+
+            //awaitPartitionMapExchange();
+            doSleep(10_000L);
+
+            startGrid(1);
+
+            //grid(0).cluster().setBaselineTopology(grid(0).cluster().topologyVersion());
+
+            //awaitPartitionMapExchange();
+            doSleep(10_000L);
+
+            IgniteInternalFuture fut = GridTestUtils.runMultiThreadedAsync(() -> {
+                while (true)
+                    for (Object obj : client.cache(DEFAULT_CACHE_NAME).query(new ScanQuery<>().setPageSize(1)))
+                        doSleep(10);
+            }, 20, "query");
+
+            doSleep(5_000L);
+
+            startGrid(2);
+
+            //grid(0).cluster().setBaselineTopology(grid(0).cluster().topologyVersion());
+
+            //awaitPartitionMapExchange();
+
+            forceCheckpoint();
+
+            doSleep(10_000L);
+
+            fut.get();
+        }
+    }
+
+    @Test
+    public void testQuery1() throws Exception {
+        startGrids(1);
+
+        grid(0).cluster().active(true);
+
+        grid(0).cluster().baselineAutoAdjustEnabled(false);
+
+        grid(0).getOrCreateCache(new CacheConfiguration<Integer, Integer>(DEFAULT_CACHE_NAME).setBackups(0));
 
         try (IgniteDataStreamer<Integer, Integer> streamer = grid(0).dataStreamer(DEFAULT_CACHE_NAME)) {
             for (int i = 0; i < 10_000; i++)
@@ -89,29 +177,52 @@ public class CachePartitionEvictionQueryTest extends GridCommonAbstractTest {
 
         IgniteClient client = Ignition.startClient(new ClientConfiguration().setAddresses("127.0.0.1:10800"));
 
-        IgniteInternalFuture fut = GridTestUtils.runMultiThreadedAsync(() -> {
-            while (true)
-                for (Object obj : client.cache(DEFAULT_CACHE_NAME).query(new ScanQuery<>().setPageSize(1)))
-                    doSleep(10);
-        }, 20, "query");
+        Iterator iter = client.cache(DEFAULT_CACHE_NAME).query(new ScanQuery<>().setPageSize(1)).iterator();
 
-        while (true) {
-            stopGrid(2);
+        if (iter.hasNext())
+            iter.next();
 
-            grid(0).cluster().setBaselineTopology(grid(0).cluster().topologyVersion());
+        startGrid(2);
 
-            awaitPartitionMapExchange();
+        grid(0).cluster().baselineAutoAdjustEnabled(false);
 
-            //doSleep(10_000L);
+        grid(0).cluster().setBaselineTopology(grid(0).cluster().topologyVersion());
 
-            startGrid(2);
+        doSleep(5_000L);
 
-            grid(0).cluster().setBaselineTopology(grid(0).cluster().topologyVersion());
+        while (iter.hasNext())
+            iter.next();
+    }
 
-            awaitPartitionMapExchange();
+    @Test
+    public void testQuery2() throws Exception {
+        startGrid(getConfiguration(getTestIgniteInstanceName(0)).setConsistentId("1"));
 
-            forceCheckpoint();
-            //doSleep(40_000L);
-        }
+        grid(0).cluster().active(true);
+
+        grid(0).cluster().baselineAutoAdjustEnabled(true);
+        grid(0).cluster().baselineAutoAdjustTimeout(0);
+
+        IgniteCache cache = grid(0).getOrCreateCache(new CacheConfiguration<Integer, Integer>(DEFAULT_CACHE_NAME)
+            .setBackups(0)
+            .setAffinity(new RendezvousAffinityFunction().setPartitions(2))
+        );
+
+        cache.put(0, 0);
+        cache.put(1, 1);
+
+        Iterator iter = grid(0).cache(DEFAULT_CACHE_NAME).query(new ScanQuery<>().setPageSize(1)).iterator();
+
+        if (iter.hasNext())
+            iter.next();
+
+        startGrid(getConfiguration(getTestIgniteInstanceName(1)).setConsistentId("0"));
+
+        awaitPartitionMapExchange();
+
+        forceCheckpoint(grid(0));
+
+        while (iter.hasNext())
+            iter.next();
     }
 }
